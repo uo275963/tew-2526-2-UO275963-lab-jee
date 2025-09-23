9cb0ccd8d66fde3999866761c219f87c5eb8e5fa
Unai <uo275963@uniovi.es>
2025-09-23 19:14:57 +0200

    2.2-Servlets con sesiones

--- src/main/java/es/tew/HolaMundoServlet.java
+++ src/main/java/es/tew/HolaMundoServlet.java
@@ -2,6 +2,7 @@ package es.tew;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Vector;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,21 +12,39 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/HolaMundo")
 public class HolaMundoServlet extends HttpServlet {
-    
+
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException,
             ServletException {
-                String nombre = (String) req.getParameter("NombreUsuario");
-                resp.setCharacterEncoding("UTF-8");
-                resp.setContentType("text/html");
-                PrintWriter out = resp.getWriter();
-               out.println("<HTML>");
-                out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
-                out.println("<BODY>");
-               
-                if ( nombre != null ){
-                out.println("<br>Hola "+nombre+"<br>");
-                }
-                out.println("Bienvenido a mi primera página Web!");
-                out.println("</BODY></HTML>");
+        String nombre = (String) req.getParameter("NombreUsuario");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<HTML>");
+        out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
+        out.println("<BODY>");
+
+ 
+
+        Vector<String> listado = (Vector<String>) req.getSession().getAttribute("listado");
+        if (listado == null) {
+            listado = new Vector<String>();
+        }
+
+        if (nombre != null) {
+            out.println("<br>Hola " + nombre + "<br>");
+            listado.addElement(nombre);
+        }
+        out.println("Bienvenido a mi primera página Web!");
+
+        req.getSession().setAttribute("listado", listado);
+        out.println("<br>");
+        out.println("Contigo, hoy me han visitado:<br>");
+        for (int i = 0; i < listado.size(); i++) {
+            out.println("<br>" + (String) listado.elementAt(i));
+
+        }
+
+        out.println("</BODY></HTML>");
+        
     }
 }
