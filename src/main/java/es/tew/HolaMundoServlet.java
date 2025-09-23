c1a8b5c4302d192eadfd96c67c21b3f1e1c8d0d0
Unai <uo275963@uniovi.es>
2025-09-23 19:34:35 +0200

    2.4-Encadenamiento de Servlets

--- src/main/java/es/tew/HolaMundoServlet.java
+++ src/main/java/es/tew/HolaMundoServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Vector;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,45 +16,8 @@ public class HolaMundoServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException,
             ServletException {
-
-        Integer contador = (Integer) getServletContext().getAttribute("contador");
-        if (contador == null) {
-            contador = 0;
-        }
-        // Establecemos el contador como atributo del context bajo el nombre
-        // contador. En caso de que ya existiera, sobreescribiría la referencia
-        // existente con la nueva.
-        contador++;
-        getServletContext().setAttribute("contador", contador);
-        String nombre = (String) req.getParameter("NombreUsuario");
-        resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println("<HTML>");
-        out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
-        out.println("<BODY>");
-
-        Vector<String> listado = (Vector<String>) req.getSession().getAttribute("listado");
-        if (listado == null) {
-            listado = new Vector<String>();
-        }
-
-        if (nombre != null) {
-            out.println("<br>Hola " + nombre + "<br>");
-            listado.addElement(nombre);
-        }
-        out.println("Bienvenido a mi primera página Web!");
-
-        req.getSession().setAttribute("listado", listado);
-        out.println("<br>");
-        out.println("Contigo, hoy me han visitado:<br>");
-        for (int i = 0; i < listado.size(); i++) {
-            out.println("<br>" + (String) listado.elementAt(i));
-
-        }
-        out.println("<br><br>" + contador + " visitas");
-
-        out.println("</BODY></HTML>");
+        RequestDispatcher dispatcher = getServletContext().getNamedDispatcher("HolaMundoVista");
+        dispatcher.forward(req, resp);
 
     }
 }
---
+++ src/main/java/es/tew/HolaMundoVistaServlet.java
@@ -0,0 +1,59 @@
+package es.tew;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Vector;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HolaMundoVista", urlPatterns = {"/HolaMundoVista"})
+public class HolaMundoVistaServlet extends HttpServlet {
+
+        public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException,
+            ServletException {
+
+        Integer contador = (Integer) getServletContext().getAttribute("contador");
+        if (contador == null) {
+            contador = 0;
+        }
+        // Establecemos el contador como atributo del context bajo el nombre
+        // contador. En caso de que ya existiera, sobreescribiría la referencia
+        // existente con la nueva.
+        contador++;
+        getServletContext().setAttribute("contador", contador);
+        String nombre = (String) req.getParameter("NombreUsuario");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<HTML>");
+        out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
+        out.println("<BODY>");
+
+        Vector<String> listado = (Vector<String>) req.getSession().getAttribute("listado");
+        if (listado == null) {
+            listado = new Vector<String>();
+        }
+
+        if (nombre != null) {
+            out.println("<br>Hola " + nombre + "<br>");
+            listado.addElement(nombre);
+        }
+        out.println("Bienvenido a mi primera página Web!");
+
+        req.getSession().setAttribute("listado", listado);
+        out.println("<br>");
+        out.println("Contigo, hoy me han visitado:<br>");
+        for (int i = 0; i < listado.size(); i++) {
+            out.println("<br>" + (String) listado.elementAt(i));
+
+        }
+        out.println("<br><br>" + contador + " visitas");
+
+        out.println("</BODY></HTML>");
+
+    }
+}
