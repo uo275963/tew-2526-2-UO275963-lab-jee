4775404690630aa26bbd16a121e16bd7f413a796
UO275963 <UO275963@uniovi.es>
2025-10-06 15:15:06 +0200

    3.10-Implementación del servicio de alumnos

--- src/main/java/es/tew/business/AlumnoService.java
+++ src/main/java/es/tew/business/AlumnoService.java
@@ -5,4 +5,7 @@ import es.tew.model.Alumno;
 
 public interface AlumnoService {
     List<Alumno> getAlumnos();
+    void saveAlumno(Alumno alumno);
+    void updateAlumno(Alumno alumno);
+    void deleteAlumno(int id);
 }
--- src/main/java/es/tew/business/AlumnoServiceImpl.java
+++ src/main/java/es/tew/business/AlumnoServiceImpl.java
@@ -21,8 +21,21 @@ import es.tew.infrastructure.PersistenceFactory;
 
 
 public class AlumnoServiceImpl implements AlumnoService {
+    @Override
     public List<Alumno> getAlumnos() {
         return PersistenceFactory.getAlumnoDAO().getAlumnos();
-
     }
+    @Override
+    public void saveAlumno(Alumno alumno) {
+        PersistenceFactory.getAlumnoDAO().saveAlumno(alumno);
+    }
+    @Override
+    public void updateAlumno(Alumno alumno) {
+        PersistenceFactory.getAlumnoDAO().updateAlumno(alumno);
+    }
+    @Override
+    public void deleteAlumno(int id) {
+        PersistenceFactory.getAlumnoDAO().deleteAlumno(id);
+    }
+
 }
