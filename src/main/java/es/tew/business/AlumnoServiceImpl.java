e77612ae2f4a72f4a5b5e60eec1c6c20212d6ee5
UO275963 <UO275963uniovi.es>
2025-10-21 19:45:58 +0200

    pequeño arreglo

--- src/main/java/es/tew/business/AlumnoServiceImpl.java
+++ src/main/java/es/tew/business/AlumnoServiceImpl.java
@@ -1,22 +1,8 @@
 package es.tew.business;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import es.tew.model.Alumno;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import es.tew.business.AlumnoService; 
 import es.tew.infrastructure.PersistenceFactory;
 
 
